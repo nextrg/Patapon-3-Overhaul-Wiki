6b82a42658136f9c008fa156098e1a5a896249fd
nextrg <oliwiercz2@wp.pl>
2024-03-22 20:43:50 +0100

    oh god

--- Scripts/filemanagement.js
+++ Scripts/filemanagement.js
@@ -430,7 +430,7 @@ function parseJSON(content, char) {
                 maskdiv.style.scale = window.innerWidth * 0.0005;
                 maskdiv.classList = "heromaskdiv";
                 if (parsedJSON[mainclasscontainer][0].mask != "") {
-                    maskimage.src = "./Assets/masks/" + parsedJSON[mainclasscontainer][0].name + "/" + parsedJSON[mainclasscontainer][0].mask + ".png";
+                    maskimage.src = "./Assets/masks/" + parsedJSON[mainclasscontainer][0].name.toLowerCase(); + "/" + parsedJSON[mainclasscontainer][0].mask + ".png";
                 }
                 else {
                     maskimage.src = "./Assets/masks/Yumiyacha/0000000098e8ef493218954c.png";
